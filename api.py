300f18f3480093a0c075952f5d23e81775f26c51
Andrew Chen <yongjhih@gmail.com>
2024-01-15 15:43:28 +0800

    wip

--- api.py
+++ api.py
@@ -1,16 +1,20 @@
+from ocrmac import ocrmac
 from flask import Flask, render_template, request
-import ddddocr
-import base64
+from flask_cors import cross_origin
+from PIL import Image
+import io, base64
 
 app = Flask(__name__)
 
 @app.route('/ocr', methods=['GET'])
+@cross_origin()
 def ocr_api():
     args = request.args
     imgBase64 = args.get('img')
-    img = base64.b64decode(f"{imgBase64}{'=' * (4 - len(imgBase64) % 4)}")
-    ocr = ddddocr.DdddOcr()
-    r = ocr.classification(imageData)
-    return ''.join(r)
+    img = Image.open(io.BytesIO(base64.decodebytes(bytes(imgBase64, "utf-8"))))
+
+    res = ocrmac.OCR(img).recognize()
+    print(res)
+    return res[0][0]
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8089)
